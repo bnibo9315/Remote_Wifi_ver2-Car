d264df6ce892604c5245db7c43794a154dade724
bnibo9315 <ngumahobao@gmail.com>
2019-11-19 10:24:51 +0700

    cmt

---
+++ Station.ino
@@ -0,0 +1,56 @@
+#include <AFMotor.h>
+
+int dir;
+AF_DCMotor motor1(1, MOTOR12_64KHZ);
+AF_DCMotor motor2(2, MOTOR12_64KHZ);
+AF_DCMotor motor(2);
+static char text_send ;
+void setup() {
+
+  Serial.begin(115200);           // mở cổng Serial monitor 9600 bps
+  Serial.println("Hello my racing");
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  motor1.setSpeed(255);  // Set motor 1 to maximum speed
+  motor2.setSpeed(255);
+}
+
+void loop() {
+  if (Serial.available()) {
+    text_send = Serial.read();
+    Serial.println(text_send);
+    dir = text_send;
+    switch (dir) {
+      case '1':
+        motor1.run(FORWARD);
+        motor2.run(FORWARD);
+        Serial.println("Up");
+        delay(100);
+        break;
+      case '2':
+        motor1.run(BACKWARD);
+        motor2.run(BACKWARD);
+        Serial.println("Down");
+        delay(100);
+        break;
+      case '3':
+        motor1.run(FORWARD);
+        motor2.run(RELEASE);
+        Serial.println("Left");
+        delay(100);
+        break;
+      case '4':
+        motor1.run(RELEASE);
+        motor2.run(FORWARD);
+        Serial.println("Right");
+        delay(100);
+        break;
+      default:
+        motor1.run(RELEASE);
+        motor2.run(RELEASE);
+        break;
+    }
+  }
+
+}
